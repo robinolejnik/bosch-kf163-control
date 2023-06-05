a645ff7476b474bd950e51720111390c2e7a27ff
Robin Olejnik <8111749+robinolejnik@users.noreply.github.com>
2023-06-05 18:13:57 +0200

    Basic trx functionality

--- main/main.c
+++ main/main.c
@@ -8,8 +8,16 @@
 #include <string.h>
 #include <stdio.h>
 
+#define STATE_RX 0
+#define STATE_TX 1
+#define STATE_SQL_CLOSE 0
+#define STATE_SQL_OPEN  1
+
 static uint16_t rx_pll = 0;
 static uint16_t tx_pll = 0;
+
+static uint8_t trx_state = STATE_RX;
+static uint8_t sql_state = STATE_SQL_CLOSE;
 
 void setrxfreq(uint32_t freq) {
 	if(freq>100000000) {
@@ -21,7 +29,7 @@ void setrxfreq(uint32_t freq) {
 }
 
 int settxfreq(uint32_t freq) {
-	if(gpio_get_level(GPIO_PTT_SENSE)) {
+	if(!gpio_get_level(GPIO_PTT_SENSE)) {
 		return -1;
 	}
 	tx_pll = freq2pll(freq);
@@ -43,14 +51,31 @@ void app_main(void) {
 
 	setrxfreq(text2freq("167940000"));
 	settxfreq(text2freq("167940000"));
+	sr_out(rx_pll);
 
 	while(1) {
-		if(gpio_get_level(GPIO_PTT_SENSE)) {
+		if(gpio_get_level(GPIO_PTT_SENSE) && trx_state == STATE_TX) {
+			trx_state = STATE_RX;
 			sr_out(rx_pll);
+			printf("RX\n");
 		}
-		else {
+		else if(!gpio_get_level(GPIO_PTT_SENSE) && trx_state == STATE_RX) {
+			trx_state = STATE_TX;
 			sr_out(tx_pll);
+			printf("TX\n");
 		}
+
+		if(!gpio_get_level(GPIO_SQL_SENSE) && sql_state == STATE_SQL_CLOSE) {
+			sql_state = STATE_SQL_OPEN;
+			gpio_set_level(GPIO_LED, 1);
+			printf("SQL open\n");
+		}
+		else if(gpio_get_level(GPIO_SQL_SENSE) && sql_state == STATE_SQL_OPEN) {
+			sql_state = STATE_SQL_CLOSE;
+			gpio_set_level(GPIO_LED, 0);
+			printf("SQL close\n");
+		}
+
 		vTaskDelay(100 / portTICK_PERIOD_MS);
 	}
 }
